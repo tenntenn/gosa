f5532d72a35fde2f66121571419c03b888f5a29e
Takuya Ueda <uedatakuya@gmail.com>
2019-02-14 18:13:02 +0900

    Add tests

--- passes/wraperrfmt/testdata/src/a/a.go
+++ passes/wraperrfmt/testdata/src/a/a.go
@@ -4,11 +4,15 @@ import "golang.org/x/xerrors"
 
 func main() {
 	var err error
-	xerrors.Errorf("message: %w", err) // OK
-	xerrors.Errorf("message:%w", err)  // want "invalid arguments"
-	xerrors.Errorf(":%w", err)         // want "invalid arguments"
-	xerrors.Errorf("%w", err)          // want "invalid arguments"
-	xerrors.Errorf("%w", nil)          // want "invalid arguments"
-	xerrors.Errorf("message: %w", nil) // want "invalid arguments"
-	xerrors.Errorf("message: %w")      // want "invalid arguments"
+	xerrors.Errorf("message: %w", err)                   // OK
+	xerrors.Errorf("message:%w", err)                    // want "invalid arguments"
+	xerrors.Errorf(":%w", err)                           // want "invalid arguments"
+	xerrors.Errorf("%w", err)                            // want "invalid arguments"
+	xerrors.Errorf("%w", nil)                            // want "invalid arguments"
+	xerrors.Errorf("message: %w")                        // want "invalid arguments"
+	xerrors.Errorf("message: %w", nil)                   // want "invalid arguments"
+	xerrors.Errorf("message: %w", nil, err)              // OK
+	xerrors.Errorf("message: %w", []interface{}{err}...) // Unsupport
+	args := []interface{}{err}
+	xerrors.Errorf("message: %w", args...) // Unsupport
 }
